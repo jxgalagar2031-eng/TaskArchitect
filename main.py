57048bece4569641293af1fe4e740d05b68ee814
jxgalagar2031-eng <jxgalagar2031@crc.pshs.edu.ph>
2026-05-06 00:02:17 +0800

    Initial commit: Added program header
    Added project title, group members, and description.

---
+++ main.py
@@ -0,0 +1,3 @@
+# Project Title: Task Architect
+# Group Members: Eli Dellosa, Xia Galagar, Laurence Cupin
+# Description: A Python program to help students manage ADHD and executive dysfunction by prioritizing tasks.
